4318a80d355d12ba9abf98439241fd066704add5
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2025-05-11 18:07:00 +0700

    Move export default declaration up

--- src/usePersistedState.ts
+++ src/usePersistedState.ts
@@ -27,7 +27,7 @@ function resolveStorageOption(
   return resolveStorageProvider(storage);
 }
 
-function usePersistedState<T>(
+export default function usePersistedState<T>(
   key: string,
   initialValue: T,
   options?: UsePersistedStateOptions
@@ -59,5 +59,3 @@ function usePersistedState<T>(
 
   return [state, setValue];
 }
-
-export default usePersistedState;
