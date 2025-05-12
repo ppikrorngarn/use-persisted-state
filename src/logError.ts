5f8e8202364624a9186b531913c2fa1e88e4d0e4
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2025-05-12 22:27:09 +0700

    Improve logError function

--- src/logError.ts
+++ src/logError.ts
@@ -1,10 +1,12 @@
 export function logError(message: string, error?: unknown) {
   const prefix = "[usePersistedState]";
+  let details = "";
+
   if (error instanceof Error) {
-    console.error(`${prefix} ${message}\n  ${error.stack}`);
+    details = `\n  ${error.stack}`;
   } else if (error !== undefined) {
-    console.error(`${prefix} ${message}\n  ${String(error)}`);
-  } else {
-    console.error(`${prefix} ${message}`);
+    details = `\n  ${String(error)}`;
   }
+
+  console.error(`${prefix} ${message}${details}`);
 }
